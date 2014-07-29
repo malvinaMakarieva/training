aa349a84b07d2a15010c271dd1ddb387b9be8505
malvinaMakarieva <malvina.v.m@gmail.com>
2014-07-29 16:57:51 +0300

    add comments

--- Introducing Java/src/test/java/com/sirma/itt/javacourse/intro/quicsortarray/QuickSortTest.java
+++ Introducing Java/src/test/java/com/sirma/itt/javacourse/intro/quicsortarray/QuickSortTest.java
@@ -4,19 +4,24 @@ import org.junit.Assert;
 import org.junit.Test;
 
 /**
+ * Class that testing QuickSoer class
+ * 
  * @author Malvina Makarieva
  */
 public class QuickSortTest {
-	QuickSort qs = new QuickSort();
+	QuickSort testSort = new QuickSort();
 	int[] testArrayNoSort = { 2, 8, 6, 4 };
-	int[] arr = { 5, 20, -45, 0, 35 };
+	int[] testInputArrat = { 5, 20, -45, 0, 35 };
 
+	/**
+	 * Method, testing the functionality of testPartition method.
+	 */
 	@Test
 	public void testPartition() {
-		int tetsPartOne = qs.partition(testArrayNoSort, 0, 3);
-		int testPartTwo = qs.partition(testArrayNoSort, 1, 3);
-		int testPartThree = qs.partition(testArrayNoSort, 0, 2);
-		int testPartFoure = qs.partition(testArrayNoSort, 2, 3);
+		int tetsPartOne = testSort.partition(testArrayNoSort, 0, 3);
+		int testPartTwo = testSort.partition(testArrayNoSort, 1, 3);
+		int testPartThree = testSort.partition(testArrayNoSort, 0, 2);
+		int testPartFoure = testSort.partition(testArrayNoSort, 2, 3);
 
 		Assert.assertEquals(3, tetsPartOne);
 		Assert.assertEquals(3, testPartTwo);
@@ -24,12 +29,15 @@ public class QuickSortTest {
 		Assert.assertEquals(3, testPartFoure);
 	}
 
+	/**
+	 * Method, testing the functionality of the QuickSort class, testPrint method.
+	 */
 	@Test
 	public void testPrint() {
-		String testPrintSortArray = qs.print(testArrayNoSort);
+		String testPrintSortArray = testSort.print(testArrayNoSort);
 		Assert.assertEquals("[2 8 6 4 ]", testPrintSortArray);
 
-		String testPrintSortArrayTwo = qs.print(arr);
+		String testPrintSortArrayTwo = testSort.print(testInputArrat);
 		Assert.assertEquals("[5 20 -45 0 35 ]", testPrintSortArrayTwo);
 	}
 }
