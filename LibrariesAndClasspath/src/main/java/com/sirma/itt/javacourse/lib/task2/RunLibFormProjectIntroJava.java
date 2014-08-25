773f134dab5fa19cb24ed742dc93cd937fe4db4a
malvinaMakarieva <malvina.v.m@gmail.com>
2014-08-25 17:04:38 +0300

    add lib form project 1 - IntroJava.jar

---
+++ LibrariesAndClasspath/src/main/java/com/sirma/itt/javacourse/lib/task2/RunLibFormProjectIntroJava.java
@@ -0,0 +1,12 @@
+package com.sirma.itt.javacourse.lib.task2;
+
+import com.sirma.itt.javacourse.intro.array.RunArrayFunction;
+
+public class RunLibFormProjectIntroJava {
+
+	public static void main(String[] args) {
+		RunArrayFunction.main(args);
+
+	}
+
+}
