54bcc836eac77910a9e2ad794af65a4a9ed28c03
malvinaMakarieva <malvina.v.m@gmail.com>
2014-08-27 12:41:34 +0300

    adding code

--- LibrariesAndClasspath/src/main/java/com/sirma/itt/javacourse/lib/task2/RunLibFormProjectIntroJava.java
+++ LibrariesAndClasspath/src/main/java/com/sirma/itt/javacourse/lib/task2/RunLibFormProjectIntroJava.java
@@ -1,10 +1,20 @@
 package com.sirma.itt.javacourse.lib.task2;
 
-import com.sirma.itt.javacourse.intro.array.RunArrayFunction;
+import com.sirma.itt.javacourse.intro.array.Array;
 
 public class RunLibFormProjectIntroJava {
 
 	public static void main(String[] args) {
-		RunArrayFunction.main(args);
+		float array[];
+
+		Array arrayFunction = new Array();
+
+		array = arrayFunction.readArrayFromConsolFloat();
+
+		System.out.println("Array elemets are " + arrayFunction.print(array));
+
+		System.out.println("Minimal element is " + arrayFunction.getMinElement(array));
+
+		System.out.println("Sum is " + arrayFunction.sum(array));
 	}
 }
