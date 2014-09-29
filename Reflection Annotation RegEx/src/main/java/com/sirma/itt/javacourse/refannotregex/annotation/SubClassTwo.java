041d4425d434781c966caa67862f37809e26965d
malvinaMakarieva <malvina.v.m@gmail.com>
2014-09-29 10:38:19 +0300

    forget to commit one class

---
+++ Reflection Annotation RegEx/src/main/java/com/sirma/itt/javacourse/refannotregex/annotation/SubClassTwo.java
@@ -0,0 +1,11 @@
+package com.sirma.itt.javacourse.refannotregex.annotation;
+
+/**
+ * The class is second successor of SuperClass.
+ * 
+ * @author Malvina Makarieva
+ */
+@SerialNumber(value = 2)
+public class SubClassTwo extends SuperClass {
+
+}
